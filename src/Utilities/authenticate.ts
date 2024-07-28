37b50ef38992b93a0032e42ec068a3cad12cbd20
Muhammad Ahsan Ali <55086927+MuhammadAichee@users.noreply.github.com>
2024-07-28 23:01:04 +0500

    Added user in authentication

--- src/Utilities/authenticate.ts
+++ src/Utilities/authenticate.ts
@@ -1,13 +1,25 @@
 export const authenticateUser = (user: any) => {
   try {
-    if (
-      user &&
-      user.email === "fawad@rumilabs.ai" &&
-      user.password === "rumilabs@pakistan123"
-    )
-      return true;
+    let findUser: IUser | undefined = users.find((x: IUser) => x.email === user.email);
+    if (findUser && findUser.password === user.password) return true;
     return false;
   } catch (e: any) {
     return false;
   }
 };
+
+interface IUser {
+  email: string;
+  password: string;
+}
+
+let users: IUser[] = [
+  {
+    email: "fawad@rumilabs.ai",
+    password: "rumilabs@pakistan123",
+  },
+  {
+    email: "user1@rumilabs.ai",
+    password: "Admin123",
+  },
+];
